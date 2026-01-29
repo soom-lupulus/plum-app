913e8e001a71506bc39561ccebf4beb7f1276bd6
余赟昊 <hope392163@163.com>
2026-01-29 19:15:13 +0800

    🐞 fix:
    修复数据库配置文件重复

--- apps/frontend/dev-dist/sw.js
+++ apps/frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-e2640689'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.gi8l29e27ng"
+    "revision": "0.1kc9pdocif"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
